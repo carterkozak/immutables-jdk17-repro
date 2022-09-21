2493451f70904e645aa346a799236c13ca241c2f
Carter Kozak <ckozak@ckozak.net>
2022-09-21 14:28:05 -0400

    initial reproducer commit

---
+++ src/main/java/net/ckozak/immutables/repro/Tuple.java
@@ -0,0 +1,40 @@
+/*
+ * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.ckozak.immutables.repro;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.immutables.value.Value;
+
+/**
+ * See <a href="https://immutables.github.io/immutable.html#tuple-style">tuple-style</a>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+@Value.Style(
+        typeAbstract = "*Tuple",
+        typeImmutable = "*",
+        visibility = Value.Style.ImplementationVisibility.PUBLIC,
+        stagedBuilder = true,
+        allParameters = true)
+@JsonSerialize
+@JsonDeserialize
+public @interface Tuple {}
